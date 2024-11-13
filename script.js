a2605a88a07504ef1702595c5c16188a71684727
andreapanico12 <andreapanico12@gmail.com>
2024-11-13 13:45:08 +0100

    feat: add distance and age const variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const distance = parseInt( prompt("Kilometers to travel") );
+const passengerAge = parseInt( prompt ("Passenger age"));
+
+console.log('Distance: ' , distance);
+console.log( 'Age', passengerAge );
