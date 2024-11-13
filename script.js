e0b3a9190a143f7c503a9ece7f469ac1f19cc943
andreapanico12 <andreapanico12@gmail.com>
2024-11-13 14:25:15 +0100

    feat: in script add condition of price in function of age

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const distance = parseInt( prompt("Kilometers to travel") );
 const passengerAge = parseInt( prompt ("Passenger age"));
 
-console.log('Distance: ' , distance);
-console.log( 'Age', passengerAge );
+console.log('Distance: ' , distance, "Km");
+console.log( 'Age:', passengerAge );
 
 const fullPrice = (distance * 0.21);
 
-console.log ('Price' , fullPrice);
+// console.log ('Price' , fullPrice);
 
 const underagePrice = ( fullPrice - (fullPrice * 0.2));
 
-console.log ( 'Underage Price' , underagePrice);
+// console.log ( 'Underage Price' , underagePrice);
 
 const overagePrice = ( fullPrice - (fullPrice * 0.4));
 
-console.log ( 'Over 65 Price' , overagePrice);
+// console.log ( 'Over 65 Price' , overagePrice);
 
 
+if ( passengerAge < 18 ) {
 
+  console.log ( "Price:",  underagePrice.toFixed(2), '€' )
+
+} else if ( passengerAge >= 65) {
+
+  console.log ( "Price:", overagePrice.toFixed(2), '€' )
+
+} else {
+
+  console.log ("Price:", fullPrice.toFixed(2), '€' )
+};
 
 
