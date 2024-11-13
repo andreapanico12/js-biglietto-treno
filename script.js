203236c9d456894129ae79d3085d4ad564d36421
andreapanico12 <andreapanico12@gmail.com>
2024-11-13 14:16:04 +0100

    feat: in script created and tested prices variables

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const passengerAge = parseInt( prompt ("Passenger age"));
 
 console.log('Distance: ' , distance);
 console.log( 'Age', passengerAge );
+
+const fullPrice = (distance * 0.21);
+
+console.log ('Price' , fullPrice);
+
+const underagePrice = ( fullPrice - (fullPrice * 0.2));
+
+console.log ( 'Underage Price' , underagePrice);
+
+const overagePrice = ( fullPrice - (fullPrice * 0.4));
+
+console.log ( 'Over 65 Price' , overagePrice);
+
+
+
+
+
